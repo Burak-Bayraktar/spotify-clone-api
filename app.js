d1b194e8105213a002311bc41dac2da5192aa78b
Burak Bayraktar <byrktrhmx@gmail.com>
2022-04-24 17:00:12 +0300

    ADD: /refreshToken endpoint

--- app.js
+++ app.js
@@ -24,12 +24,42 @@ app.get('/accessToken', async (req,res) => {
             'client_secret': process.env.CLIENT_SECRET
         }), headers)
 
+        res.status(200)
         res.json({
-            'access-token': result.data['access_token']
+            'access-token': result.data['access_token'],
+            'refresh-token': result.data['refresh_token']
         })
     } catch (error) {
+        res.status(error.response.status)
         res.json(error)
     }
+})
+
+app.get('/refreshToken', async (req,res) => {
+    const refreshToken = req.query['refresh_token']
+    
+    const headers = {
+        'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64')),
+        'Content-Type': 'application/x-www-form-urlencoded'
+    };
+
+    try {
+        const result = await axios.post('https://accounts.spotify.com/api/token', qs.stringify({
+            'grant_type': 'refresh_token',
+            'refresh_token': refreshToken,
+            'client_id': process.env.CLIENT_ID,
+            'client_secret': process.env.CLIENT_SECRET
+        }), headers);
+
+        res.status(200)
+        res.json({
+            'access-token': result.data['access_token'],
+        })
+    } catch (error) {
+        res.status(error.response.status)
+        res.json(error)
+    }
+
 })
 
 app.get('/getCurrentUser', async (req, res) => {
@@ -39,9 +69,11 @@ app.get('/getCurrentUser', async (req, res) => {
             'Content-Type': 'application/json'
         }})
 
+        res.status(200)
         res.json(result.data)
     } catch (error) {
-        console.log(error)
+        res.status(error.response.status)
+        res.json(error)
     }
 })
 
